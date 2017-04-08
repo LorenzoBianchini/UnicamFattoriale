dea0c6c25dc9b9246bd25e3b5f44c03eeb9c139a
Lorenzo Bianchini <Lorenzo Bianchini>
2017-04-08 17:23:17 +0200

    Impostato controllo di input troppo grande o non intero con relativo messaggio di errore. Inserito il loop finché non è inserito un input non consentito e corretto il calcolo del fattoriale.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace UnicamFattoriale
 {
@@ -8,26 +9,40 @@ namespace UnicamFattoriale
         {
             //Stampo dei messaggi di cortesia per l'utente, così lo guido all'utilizzo del programma
             Console.WriteLine("Questo programma calcola il fattoriale di un numero");
-            Console.Write("Digita un numero e premi invio: ");
+            
+            
+            Console.Write("Digita un numero intero e premi invio: ");
 
             //Console.ReadLine si mette in attesa che l'utente digiti qualcosa e prema invio.
             //Quando ha premuto invio, il testo che ha digitato lo assegno ad una variabile di tipo string.
             string testoDigitatoDallUtente = Console.ReadLine();
-
+            
             //Interpreto il testo come un numero intero
             //Attenzione: questa istruzione fallirà se l'utente ha digitato un testo
             //che non è interpretabile come un numero! (Es. ABCD)
-            int numero = int.Parse(testoDigitatoDallUtente);
+            bool controlloVariabileInt = false;
+            while(controlloVariabileInt = int.TryParse(testoDigitatoDallUtente, out int numero )){
 
-            //Calcolo il fattoriale: qui ci sono degli errori!
-            int risultato = 0;
-            while (numero >= 0) {
-                risultato *= numero;
-                numero--;
+                BigInteger risultato = numero;            
+                int i = numero-1;            
+                if(numero!=0){
+                    while (i > 0) {
+                        risultato *= i;
+                        i--;                                
+                    }
+                }
+                else{
+                    risultato = 1;
+                }
+                //Stampo il risultato
+                Console.WriteLine($"Il fattoriale di {numero} e' {risultato}");
+
+                Console.Write("Digita un numero intero e premi invio: ");
+
+                testoDigitatoDallUtente = Console.ReadLine();
             }
-
-            //Stampo il risultato
-            Console.WriteLine($"Il fattoriale di {numero} e' {risultato}");
+            if(!controlloVariabileInt)
+                Console.WriteLine("Spiacente, non posso calcolare il fattoriale per questo valore.");
             //Attendo che l'utente prema un tasto prima di uscire, altrimenti 
             Console.ReadKey();
         }
